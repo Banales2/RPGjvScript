a362b0e89a33d4c1232027898912692f00e51219
Banales2 <A01642425@tec.mx>
2025-07-28 10:46:13 -0600

    Amen

---
+++ script.js
@@ -0,0 +1,79 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let intentory = ['stick'];
+
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+const button3 = document.querySelector('#button3');
+const text = document.querySelector('#text');
+const xpText = document.querySelector('#xpText');
+const healthText = document.querySelector('#healthText');
+const goldText = document.querySelector('#goldText');
+const monsterStats = document.querySelector('#monsterStats');
+const monsterHealthText = document.querySelector('#monsterHealthText');
+const monsterNameText = document.querySelector('#monsterNameText');
+
+// Initialize the game state
+const locations = [
+    {
+        name: 'town square',
+        'button text': ['Go to store', 'Go to cave', 'Fight dragon'],
+        'button functions': [goStore, goCave, fightDragon],
+        text: 'You are in the town square. You can see a sign for the store and a path leading to the cave.'
+    },
+    {
+        name: 'store',
+        'button text': ['Buy 10 health (10 gold)', 'Buy weapon (30 gold)', 'Go to town square'],
+        'button functions': [buyHealth, buyWeapon, goTown],
+        text: 'You enter the store.'
+    },
+    {
+        name: 'cave',
+        'button text': ['Fight slime', 'Fight fanged beast', 'Go to town square'],
+        'button functions': [fightSlime, fightBeast, goTown],
+        text: 'You are in a dark cave. You can hear a monster growling.'
+    }
+];
+
+//initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function update(location) {
+    text.innerText = location.text;
+    button1.innerText = location['button text'][0];
+    button2.innerText = location['button text'][1];
+    button3.innerText = location['button text'][2];
+    
+    button1.onclick = location['button functions'][0];
+    button2.onclick = location['button functions'][1];
+    button3.onclick = location['button functions'][2];
+}
+
+function goTown() {
+    update(locations[0]);
+}
+
+function goStore() {
+    update(locations[1]);
+}
+
+function goCave() {
+    update(locations[2]);
+}
+
+function fightDragon() {
+    console.log('Fighting dragon');
+}
+
+function buyHealth() {
+
+}
+
+function buyWeapon() {}
+
